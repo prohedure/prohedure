4b360152c469cd41082e88a1c5268312263d6999
zhouzhou <1783893088@qq.com>
2022-12-12 10:55:03 +0800

    路书功能

--- js/secArea.js
+++ js/secArea.js
@@ -14,46 +14,251 @@ function bondLayers(map) {
 
     // 绑定兴趣点
     var addPoi = false
-
     $('#poiLayer input:radio[name="poiInput"]').click(function () {
 
-        if(addPoi) {
+        if (addPoi) {
 
 
 
             return
         }
-        
+
         if ($('#poiLayer input:radio[name="poiInput"]').prop("checked")) {
 
             addPoi = true
 
             // 选中后
-           console.log(map._displayOptions.poi = true)
-           map.setZoom(9)
+            console.log(map._displayOptions.poi = true)
+            map.setZoom(9)
 
-        } 
+        }
 
     });
 
     //绑定交通页面
     var addJiaotong = false
-
     $('#jiaotongLayer input:radio[name="jiaotong"]').click(function () {
 
-        
-        if(addJiaotong) {
-            map.setTrafficOff(); 
-            $('#jiaotongLayer input:radio[name="jiaotong"]').attr("checked",false);
+
+        if (addJiaotong) {
+            map.setTrafficOff();
+            $('#jiaotongLayer input:radio[name="jiaotong"]').attr("checked", false);
             addJiaotong = false
             return
         }
-        
+
         if ($('#jiaotongLayer input:radio[name="jiaotong"]').prop("checked")) {
 
             addJiaotong = true
             map.setTrafficOn();
-        } 
+        }
 
     });
+
+
+    // 绑定geojson
+    addGeoJsonFunc(map)
+
+
+    // 绑定wms图层
+    wmsBond(map) 
+
+
+
+    // 绑定geojson
+    function addGeoJsonFunc(map) {
+        //绑定geojsond页面
+        var addGeojson = false
+
+        var bjRegionLayer;
+
+        $('#geojsonLayer input:radio[name="geojsonData"]').click(function () {
+
+
+            if (addGeojson) {
+                $('#geojsonLayer input:radio[name="geojsonData"]').attr("checked", false);
+                addGeojson = false
+
+
+                // 按钮取消，执行
+
+
+                bjRegionLayer && map.removeGeoJSONLayer(bjRegionLayer);
+                map.removeOverlay(label);
+
+                return
+            }
+
+            if ($('#geojsonLayer input:radio[name="geojsonData"]').prop("checked")) {
+
+                addGeojson = true
+
+                // 按钮选中，执行
+                //宝鸡市的geojson数据
+                $.ajax({
+                    type: "GET",
+                    url: "https://geo.datav.aliyun.com/areas_v3/bound/610300_full.json",
+                    dataType: 'json',
+                    success: (data) => {
+                        console.log(data);
+
+                        addGeoLayer1(data)
+
+
+                    }
+                })
+
+            }
+
+
+            // 添加宝鸡图层
+            function addGeoLayer1(data) {
+                //   let myPolygonStyle = {
+                //     strokeColor: 'darkred',
+                //     strokeWeight: 4,
+                //     fillOpacity: 0.6
+                //   }
+
+                //     let geoJsonSetting = {
+                //         dataSource: data,
+                //         reference: "GCJ02",
+                //         polygonStyle: myPolygonStyle,
+                //         minZoom: 7,        
+                //         maxZoom:19,         
+                //         level: -10,
+                //         visible: true
+                //     }
+
+                //     //构建图层,地图展示
+                //     let geoJsonLayer = BMapGL.GeoJSONLayer('宝鸡市',geoJsonSetting)
+                //     map.addGeoJSONLayer(geoJsonLayer)
+
+                var colorBand = ['#CCFF99', '#FFFFFF', '#99CCFF', '#FFCC99', '#FFFFCC', '#99CCCC' ,'#CCFFFF' ,'#66CCFF', '#CC9999', '#FFFF00' ,'#CCCCCC'];
+
+                bjRegionLayer = new BMapGL.GeoJSONLayer('bj-child', {
+                    reference: 'GCJ02',
+                    dataSource: data,         // 北京各区数据
+                    level: -10,                // 显示层级，由于系统内部问题，GeoJSONLayer图层等级使用负数表达，负数越大层级越高，默认-99
+                    minZoom: 7,               // 设置图层显示的地图最小等级
+                    maxZoom: 19,                // 设置图层显示的地图最大等级
+
+                    polygonStyle: function (properties) {
+                        var index = properties.subFeatureIndex || 0;
+                        return {
+                            fillColor: colorBand[index]
+                        }
+                    },
+
+                });
+                map.addGeoJSONLayer(bjRegionLayer);
+                map.centerAndZoom(new BMapGL.Point(107.2, 34.3), 10)
+
+            }
+
+
+        });
+    }
+
+
+    // 绑定wms图层
+    function wmsBond(map) {
+
+
+        var wms
+        var addWms = false
+        $('#wmsLayer input:radio[name="wmsData"]').click(function () {
+    
+            if (addWms) {
+    
+            $('#wmsLayer input:radio[name="wmsData"]').attr("checked", false);
+            addWms = false
+
+            
+            wms && map.removeTileLayer(wms);
+            map.removeOverlay(label);
+
+
+
+            return
+            }
+    
+            if ($('#wmsLayer input:radio[name="wmsData"]').prop("checked")) {
+    
+
+                addWms = true
+                map.setTilt(0); 
+
+
+                 wms = new BMapGL.XYZLayer({
+                    useThumbData: true,
+                    tileUrlTemplate: 'https://ows.mundialis.de/services/service?&service=WMS&' +
+                        'request=GetMap&layers=TOPO-WMS%2COSM-Overlay-WMS&styles=&format=image%2Fjpeg&' +
+                        'transparent=false&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox=[b]',
+                });
+                map.addTileLayer(wms);
+                var bd = new BMapGL.Boundary();
+                bd.get('陕西', function (rs) {
+                    wms.addBoundary(rs.boundaries);
+                });
+
+                map.centerAndZoom( new BMapGL.Point(107, 34),6); 
+
+        
+    
+            }
+    
+        });
+
+    }
+
+
+    // // 绑定tms图层
+    // function wmtsBond(map) {
+
+
+    //     var wms
+    //     var addWms = false
+    //     $('#wmtsLayer input:radio[name="wmtsData"]').click(function () {
+    
+    //         if (addWms) {
+    
+    //         $('#wmtsLayer input:radio[name="wmtsData"]').attr("checked", false);
+    //         addWms = false
+
+            
+    //         // wms && map.removeTileLayer(wms);
+    //         // map.removeOverlay(label);
+
+
+
+    //         return
+    //         }
+    
+    //         if ($('#wmtsLayer input:radio[name="wmtsData"]').prop("checked")) {
+    
+
+    //             addWms = true
+    //             map.setTilt(0); 
+
+
+    //              wms = new BMapGL.XYZLayer({
+    //                 useThumbData: true,
+    //                 tileUrlTemplate: 'https://localhost:6443/arcgis/rest/services/MapService_busi/MapServer/WMTS?/tile/1.0.0/MapService_busi/default/EPSG:4326/EPSG:4326:1/1/1.png',
+                  
+
+    //                 extentCRSIsWGS84: true,
+    //             });
+    //             map.addTileLayer(wms);
+
+
+    //             map.centerAndZoom( new BMapGL.Point(107, 34),6); 
+
+        
+    
+    //         }
+    
+    //     });
+
+    // }
+
 }
--- js/thirArea.js
+++ js/thirArea.js
@@ -16,6 +16,9 @@ function bondMoreFunc(map) {
 
     // 绑定轨迹动画
     trackAnim(map)
+
+    // 绑定路书动画
+    bondLushuAnim(map)
 }
 
 
@@ -148,7 +151,7 @@ function animAct(map) {
 }
 
 
-
+// ————————————————————————————————————轨迹动画
 // 绑定轨迹动画
 function trackAnim(map) {
     //绑定交通页面
@@ -248,9 +251,98 @@ function animGuijiAct(map) {
     }).catch(
         err => alert(err)
     );
-    
+
 
     return promise
+
+
+}
+
+
+// ————————————————————————————————————路书动画
+// 绑定路书动画 
+function bondLushuAnim(map) {
+    //绑定交通页面
+    var addLushu = false
+
+    var lushu
+
+    $('#lushuLayer input:radio[name="animLushu"]').click(
+        async () => {
+
+        if (addLushu) {
+            $('#lushuLayer input:radio[name="animLushu"]').attr("checked", false);
+            addLushu = false
+
+            // 取消按钮 执行
+            lushu.stop();
+
+            return
+        }
+        if ($('#lushuLayer input:radio[name="animLushu"]').prop("checked")) {
+
+            addLushu = true
+
+           if(typeof lushu != 'object'){
+            lushu = await animLushuAct(map)
+
+           }
+
+
+            // 选中按钮 执行
+            lushu.start();
+
+        }
+
+    })
+}
+
+
+
+async function animLushuAct(map) {
+    var lushu = await new Promise(func)
+    // 实例化一个驾车导航用来生成路线
+    function func(resolve) {
+
+        var drv = new BMapGL.DrivingRoute('北京', {
+            onSearchComplete: function (res) {
+                if (drv.getStatus() == BMAP_STATUS_SUCCESS) {
+                    var plan = res.getPlan(0);
+                    var arrPois = [];
+                    for (var j = 0; j < plan.getNumRoutes(); j++) {
+                        var route = plan.getRoute(j);
+                        arrPois = arrPois.concat(route.getPath());
+                    }
+                    map.addOverlay(new BMapGL.Polyline(arrPois, { strokeColor: '#111' }));
+                    map.setViewport(arrPois);
+
+
+
+                    lushu = new BMapGLLib.LuShu(map, arrPois, {
+                        defaultContent: '', // "从天安门到百度大厦"
+                        autoView: true, // 是否开启自动视野调整，如果开启那么路书在运动过程中会根据视野自动调整
+                        icon: new BMapGL.Icon('./images/car.png', new BMapGL.Size(32, 32), { anchor: new BMapGL.Size(10, 10) }),
+                        speed: 500,
+                        enableRotation: true, // 是否设置marker随着道路的走向进行旋转
+                        // landmarkPois: [
+                        //     {lng:116.314782, lat:39.913508, html:'加油站', pauseTime:2},
+                        //     {lng:116.315391, lat:39.964429, html:'高速公路收费<div><img src="//map.baidu.com/img/logo-map.gif"/></div>', pauseTime:3},
+                        //     {lng:116.381476, lat:39.974073, html:'肯德基早餐', pauseTime:2}
+                        // ]
+                    });
+                    resolve(lushu)
+                }
+            }
+        });
+        var start = new BMapGL.Point(116.404844, 40);
+        var end = new BMapGL.Point(116.308102, 40.056057);
+        drv.search(start, end);
+    }
+
+
+
+
+    return lushu
 
 
 }
